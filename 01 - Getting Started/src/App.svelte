a73163d03b08ff6c4c053d7a82b79ab583aefe57
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-09 20:08:29 +0330

    Add files via upload

---
+++ 01 - Getting Started/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let name = "Max";
+  let age = 30;
+  function incrementAge() {
+    age += 1;
+  }
+</script>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
