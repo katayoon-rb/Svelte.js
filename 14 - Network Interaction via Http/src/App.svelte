c8e9948d276ea07d3b2ef36ba896b3a2a38d0470
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-01-15 13:08:06 +0330

    Add files via upload

---
+++ 14 - Network Interaction via Http/src/App.svelte
@@ -0,0 +1,74 @@
+<script>
+  import hobbyStore from "./hobby-store.js";
+
+  let hobbies = [];
+  let hobbyInput;
+  let isLoading = false;
+
+  isLoading = true;
+  let getHobbies = fetch(
+    "https://svelte-course-37ef4-default-rtdb.firebaseio.com/hobbies.json"
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed!");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      isLoading = false;
+      hobbyStore.setHobbies(Object.values(data));
+      let keys = Object.keys(data);
+      console.log(keys);
+
+      for (const key in data) {
+        console.log(key, data[key]);
+      }
+      return hobbies;
+    })
+    .catch((err) => {
+      isLoading = false;
+      console.log(err);
+    });
+
+  function addHobby() {
+    hobbyStore.addHobby(hobbyInput.value);
+
+    isLoading = true;
+    fetch(
+      "https://svelte-course-37ef4-default-rtdb.firebaseio.com/hobbies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(hobbyInput.value),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        isLoading = false;
+        if (!res.ok) {
+          throw new Error("Failed!");
+        }
+        alert("Saved Data!");
+      })
+      .catch((err) => {
+        isLoading = false;
+        console.log(err);
+      });
+  }
+</script>
+
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput} />
+<button on:click={addHobby}>Add Hobby</button>
+
+{#if isLoading}
+  <p>Loading...</p>
+{:else}
+  <ul>
+    {#each $hobbyStore as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{/if}
